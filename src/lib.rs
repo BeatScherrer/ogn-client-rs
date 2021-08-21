69335cf0a7a69da744d22c4ddf8d62a50ce7a2b9
Beat Scherrer <beat.scherrer@gmail.com>
2021-08-21 17:17:50 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -25,32 +25,32 @@ impl APRSClient {
   pub fn run(&mut self) {
     info!("starting the client...");
 
-    // create the buffer reader first which handles readline
-    let mut buffer = String::new();
-
-    info!("starting the listening loop...");
     loop {
       self.send_heart_beat();
 
-      let result = self.m_reader.read_line(&mut buffer);
-      debug!("{:?}", result);
+      let result = self.read().unwrap();
+      info!("{:?}", result);
     }
   }
 
   /// Send bytes and return the answer
-  pub fn send_message(&mut self, message: &str) -> Result<String, std::io::Error> {
-    self.m_writer.write_all(message.as_bytes())?;
+  pub fn send_message(&mut self, message: &str) -> Result<(), std::io::Error> {
+    let mut full_message = String::new();
+    full_message.push_str(message);
+    full_message.push_str("\n");
+
+    self.m_writer.write_all(full_message.as_bytes())?;
     self.m_writer.flush()?;
 
-    self.read()
+    Ok(())
   }
-
+  
   pub fn send_heart_beat(&mut self) {
-    self.send_message("#keepalive\n").unwrap();
-    debug!("sent heartbeat");
+    self.send_message("#keepalive").unwrap();
   }
 
   fn read(&mut self) -> Result<String, std::io::Error> {
+    debug!("reading message ...");
     
     let mut string_buffer = String::new();
 
@@ -66,18 +66,30 @@ impl Drop for APRSClient{
   }
 }
 
-struct LoginData {
+pub struct LoginData {
   pub user_name: String,
-  pub pass_code: Option<String>,
+  pub pass_code: String,
   pub app_name: String,
   pub app_version: String,
+}
+
+impl LoginData {
+  pub fn new(user_name: &str, pass_code: Option<&str>, app_name: &str, app_version: Option<&str>) -> LoginData {
+
+    Self{
+      user_name: user_name.to_string(),
+      pass_code: pass_code.get_or_insert("-1").to_string(),
+      app_name: app_name.to_string(),
+      app_version: env!("CARGO_PKG_VERSION").to_string()
+    }
+  }
 }
 
 fn create_aprs_login(mut login_data: LoginData) -> String {
   login_data.pass_code.get_or_insert(String::from("-1"));
 
   format!(
-    "user {} pass {} vers {} {}\n",
+    "user {} pass {} vers {} {}",
     login_data.user_name, login_data.pass_code.unwrap(), login_data.app_name, login_data.app_version
   )
 }
