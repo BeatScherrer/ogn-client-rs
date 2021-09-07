e4039a3379269446d0cbb404f59411fe5daea489
Beat Scherrer <b.scherrer@mt-robot.com>
2021-09-07 22:48:50 +0200

    minor example adjustments

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,27 @@ fn main() -> Result<(), Error> {
 
   log4rs::init_config(config).unwrap();
 
+  // ------------------------------------------------------------------------------
+  // Here comes the interesting part
+  // ------------------------------------------------------------------------------
   let callback = |message: &str| {
     println!("callback: {}", message);
   };
 
+  // pass the callback to the client!
   let mut client = APRSClient::new("aprs.glidernet.org", PORT::FULLFEED, Box::new(callback));
 
+  // log into the network
   let login_data = LoginData::new().user_name("Beat");
-
   client.login(login_data)?;
 
+  // example of sending a position message
   // client.send_message("user AE5PL-TS pass -1 vers testsoftware 1.0_05 filter r/33.25/-96.5/50").unwrap();
 
-  client.run();
+  // example of sending a status message
+  // TODO
 
+  // TODO think about where to dispatch the reading
+  client.run();
   Ok(())
 }
